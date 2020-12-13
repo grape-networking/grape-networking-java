74d411e0952764b607cb97ae3bb7e464049355c3
Jason Ernst <ernstjason1@gmail.com>
2020-12-12 22:16:27 -0800

    Added udp and tcp tests

--- lib/src/test/java/network/grape/lib/transport/tcp/TcpTest.java
+++ lib/src/test/java/network/grape/lib/transport/tcp/TcpTest.java
@@ -1,4 +1,32 @@
 package network.grape.lib.transport.tcp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import network.grape.lib.PacketHeaderException;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
 public class TcpTest {
+  TcpHeader testTcpHeader() {
+    return new TcpHeader(34645, 443, 1, 0,
+        (short) 0, 2, 1024, 0, 0, new ArrayList<>());
+  }
+
+  // disabled until the toByteArray is properly implemented.
+  @Disabled
+  @Test
+  public void serialDeserialize() throws PacketHeaderException {
+    TcpHeader tcpHeader = testTcpHeader();
+    byte[] buf = tcpHeader.toByteArray();
+    ByteBuffer buffer = ByteBuffer.allocate(buf.length);
+    buffer.put(buf);
+    buffer.rewind();
+
+    TcpHeader tcpHeader1 = TcpHeader.parseBuffer(buffer);
+
+    assertEquals(tcpHeader, tcpHeader1);
+  }
 }
--- lib/src/test/java/network/grape/lib/transport/udp/UdpTest.java
+++ lib/src/test/java/network/grape/lib/transport/udp/UdpTest.java
@@ -1,4 +1,40 @@
 package network.grape.lib.transport.udp;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+
+import java.nio.ByteBuffer;
+import network.grape.lib.PacketHeaderException;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Test the UdpHeader class.
+ */
 public class UdpTest {
+
+  UdpHeader testUdpHeader() {
+    return new UdpHeader(34476, 9999, 1024, 0);
+  }
+
+  @Test
+  public void serialDeserialize() throws PacketHeaderException {
+    UdpHeader udpHeader = testUdpHeader();
+    byte[] buf = udpHeader.toByteArray();
+    ByteBuffer buffer = ByteBuffer.allocate(buf.length);
+    buffer.put(buf);
+    buffer.rewind();
+
+    UdpHeader udpHeader1 = UdpHeader.parseBuffer(buffer);
+
+    assertEquals(udpHeader, udpHeader1);
+  }
+
+  @Test
+  public void underflow() {
+    ByteBuffer buf = ByteBuffer.allocate(0);
+    assertThrows(PacketHeaderException.class, () -> {
+      UdpHeader.parseBuffer(buf);
+    });
+  }
 }
