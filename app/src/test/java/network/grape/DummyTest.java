bd90ab7ed669c9e440aa7ccb093773703af8ba71
Jason Ernst <ernstjason1@gmail.com>
2020-12-24 10:57:00 -0800

    Fixed android test coverage

---
+++ app/src/test/java/network/grape/DummyTest.java
@@ -0,0 +1,13 @@
+package network.grape;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+
+public class DummyTest {
+  @Test
+  public void dummyTest() {
+    assertTrue(true);
+  }
+}
