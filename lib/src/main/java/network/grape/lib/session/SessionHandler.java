f8f130204587b87c79d61e17a3c099b31dcf6179
Jason Ernst <ernstjason1@gmail.com>
2020-12-27 09:24:57 -0800

    Disabled ip debug filtering

--- lib/src/main/java/network/grape/lib/session/SessionHandler.java
+++ lib/src/main/java/network/grape/lib/session/SessionHandler.java
@@ -71,15 +71,15 @@ public class SessionHandler {
       throw new PacketHeaderException("Got a packet which isn't Ip4 or Ip6: " + version);
     }
 
-    if (!ipHeader.getDestinationAddress().equals(Inet4Address.getByName("192.168.1.20"))
-        && (!ipHeader.getSourceAddress().equals(Inet4Address.getByName("192.168.1.20")))) {
-      //logger.info(ipHeader.getDestinationAddress().toString() + " "
-      // + ipHeader.getSourceAddress().toString());
-      return;
-    }
-    logger.info("GOT TRAFFIC TO/FROM 192.168.1.20: " + ipHeader.getDestinationAddress().toString() + " "
-        + ipHeader.getSourceAddress().toString());
-    logger.info("PROTO: " + ipHeader.getProtocol());
+//    if (!ipHeader.getDestinationAddress().equals(Inet4Address.getByName("192.168.1.20"))
+//        && (!ipHeader.getSourceAddress().equals(Inet4Address.getByName("192.168.1.20")))) {
+//      //logger.info(ipHeader.getDestinationAddress().toString() + " "
+//      // + ipHeader.getSourceAddress().toString());
+//      return;
+//    }
+//    logger.info("GOT TRAFFIC TO/FROM 192.168.1.20: " + ipHeader.getDestinationAddress().toString() + " "
+//        + ipHeader.getSourceAddress().toString());
+//    logger.info("PROTO: " + ipHeader.getProtocol());
 
     final TransportHeader transportHeader;
     if (ipHeader.getProtocol() == TransportHeader.UDP_PROTOCOL) {
