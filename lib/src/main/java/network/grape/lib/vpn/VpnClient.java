ddb8d3e014a86b53a2778e01975204297e5dfb9d
Jason Ernst <ernstjason1@gmail.com>
2021-06-09 07:57:20 -0700

    Implemented the start of a VPN client that can be used in Java or Android

--- lib/src/main/java/network/grape/lib/vpn/VpnClient.java
+++ lib/src/main/java/network/grape/lib/vpn/VpnClient.java
@@ -1,15 +1,48 @@
 package network.grape.lib.vpn;
 
-//import io.grpc.ManagedChannel;
-//import io.grpc.ManagedChannelBuilder;
-//import network.grape.vpn.VpnGrpc;
-//import network.grape.vpn.VpnService;
-//
-//public class VpnClient {
-//    public void connect() {
-//        VpnService.HelloRequest helloRequest = VpnService.HelloRequest.newBuilder().setName("Test").build();
-//        ManagedChannel mChannel = ManagedChannelBuilder.forAddress("www.jasonernst.com", 8888).usePlaintext().build();
-//        VpnGrpc.VpnBlockingStub futureStub = VpnGrpc.newBlockingStub(mChannel);
-//        VpnService.HelloResponse response = futureStub.sayHello(helloRequest);
-//    }
-//}
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+public class VpnClient {
+
+    private InetAddress serverAddress;
+    private int serverPort;
+    private VpnForwardingWriter vpnWriter;
+    private VpnForwardingReader vpnReader;
+    private Thread vpnWriterThread;
+    private Thread vpnReaderThread;
+    private volatile boolean running;
+
+    public VpnClient(String host, int port, VpnForwardingWriter vpnWriter, VpnForwardingReader vpnReader) throws UnknownHostException {
+        this.serverAddress = InetAddress.getByName(host);
+        this.serverPort = port;
+        this.vpnReader = vpnReader;
+        this.vpnWriter = vpnWriter;
+    }
+
+    public void start() {
+        vpnWriterThread = new Thread(vpnWriter);
+        vpnWriterThread.start();
+
+        vpnReaderThread = new Thread(vpnReader);
+        vpnReaderThread.start();
+    }
+
+    public void shutdown() {
+        if (vpnReader != null) {
+            vpnReader.shutdown();
+        }
+
+        if (vpnReaderThread != null) {
+            vpnReaderThread.interrupt();
+        }
+
+        if (vpnWriter != null) {
+            vpnWriter.shutdown();
+        }
+
+        if (vpnWriterThread != null) {
+            vpnWriterThread.interrupt();
+        }
+    }
+}
